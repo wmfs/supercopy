fa4117206b88843c7ae0b179d1a9f6cb94e51b91
Jez Higgins <jez@jezuk.co.uk>
2020-08-28 16:23:13 +0100

    feat: Added option.quote to change the quote default from " to whatever.

--- test/supercopy-csv-tests.js
+++ test/supercopy-csv-tests.js
@@ -38,52 +38,65 @@ describe('Supercopy tests', function () {
     for (const filename of ['uninstall.sql', 'install.sql']) { await client.runFile(path.resolve(__dirname, path.join('fixtures', 'scripts', filename))) }
   })
 
-  describe('good data', () => {
-    it('Supercopy some people', function () {
-      return supercopy(
-        {
-          sourceDir: path.resolve(__dirname, './fixtures/input-data/people'),
-          topDownTableOrder: ['adults', 'children'],
-          headerColumnNamePkPrefix: '.',
-          client: client,
-          schemaName: 'supercopy_test',
-          debug: true
-        }
-      )
-    })
+  const goodOptions = {
+    topDownTableOrder: ['adults', 'children'],
+    headerColumnNamePkPrefix: '.',
+    schemaName: 'supercopy_test',
+    debug: true
+  }
 
-    it('Verify adult rows', async () => {
-      const result = await client.query('select adult_no,first_name,last_name from supercopy_test.adults order by adult_no')
-      expect(result.rows).to.eql(
-        [
-          { adult_no: 10, first_name: 'Homer', last_name: 'Simpson' },
-          { adult_no: 20, first_name: 'Marge', last_name: 'Simpson' },
-          { adult_no: 30, first_name: 'Maud', last_name: 'Flanders' },
-          { adult_no: 40, first_name: 'Ned', last_name: 'Flanders' },
-          { adult_no: 50, first_name: 'Seymour', last_name: 'Skinner' },
-          { adult_no: 60, first_name: 'Charles', last_name: 'Burns' },
-          { adult_no: 80, first_name: 'Clancy', last_name: 'Wiggum' },
-          { adult_no: 90, first_name: 'Abraham', last_name: 'Simpson' },
-          { adult_no: 100, first_name: 'Mona', last_name: 'Simpson' }
-        ]
-      )
-    })
+  const testConfigs = [
+    ['people'],
+    ['people-quote', '\'']
+  ]
 
-    it('Verify modified children rows', async () => {
-      const result = await client.query('select child_no,first_name,last_name from supercopy_test.children order by child_no')
-      expect(result.rows).to.eql(
-        [
-          { child_no: 10, first_name: 'Lisa', last_name: 'Simpson' },
-          { child_no: 20, first_name: 'Bart', last_name: 'Simpson' },
-          { child_no: 30, first_name: 'Maggie', last_name: 'Simpson' },
-          { child_no: 40, first_name: 'Rod', last_name: 'Flanders' },
-          { child_no: 50, first_name: 'Todd', last_name: 'Flanders' },
-          { child_no: 60, first_name: 'Nelson', last_name: 'Muntz' },
-          { child_no: 70, first_name: 'Milhouse', last_name: 'Van Houten' }
-        ]
-      )
+  for (const [goodFixture, quoted] of testConfigs) {
+    describe(`${goodFixture} data`, () => {
+      it('reset test data', async () => {
+        for (const filename of ['uninstall.sql', 'install.sql']) { await client.runFile(path.resolve(__dirname, path.join('fixtures', 'scripts', filename))) }
+      })
+
+      it('Supercopy some people', function () {
+        goodOptions.client = client
+        goodOptions.sourceDir = path.resolve(__dirname, './fixtures/input-data', goodFixture)
+        goodOptions.quote = quoted
+
+        return supercopy(goodOptions)
+      })
+
+      it('Verify adult rows', async () => {
+        const result = await client.query('select adult_no,first_name,last_name from supercopy_test.adults order by adult_no')
+        expect(result.rows).to.eql(
+          [
+            { adult_no: 10, first_name: 'Homer', last_name: 'Simpson' },
+            { adult_no: 20, first_name: 'Marge', last_name: 'Simpson' },
+            { adult_no: 30, first_name: 'Maud', last_name: 'Flanders' },
+            { adult_no: 40, first_name: 'Ned', last_name: 'Flanders' },
+            { adult_no: 50, first_name: 'Seymour', last_name: 'Skinner' },
+            { adult_no: 60, first_name: 'Charles', last_name: 'Burns' },
+            { adult_no: 80, first_name: 'Clancy', last_name: 'Wiggum' },
+            { adult_no: 90, first_name: 'Abraham', last_name: 'Simpson' },
+            { adult_no: 100, first_name: 'Mona', last_name: 'Simpson' }
+          ]
+        )
+      })
+
+      it('Verify modified children rows', async () => {
+        const result = await client.query('select child_no,first_name,last_name from supercopy_test.children order by child_no')
+        expect(result.rows).to.eql(
+          [
+            { child_no: 10, first_name: 'Lisa', last_name: 'Simpson' },
+            { child_no: 20, first_name: 'Bart', last_name: 'Simpson' },
+            { child_no: 30, first_name: 'Maggie', last_name: 'Simpson' },
+            { child_no: 40, first_name: 'Rod', last_name: 'Flanders' },
+            { child_no: 50, first_name: 'Todd', last_name: 'Flanders' },
+            { child_no: 60, first_name: 'Nelson', last_name: 'Muntz' },
+            { child_no: 70, first_name: 'Milhouse', last_name: 'Van Houten' }
+          ]
+        )
+      })
     })
-  })
+  } // for ...
 
   describe('bad data', () => {
     it('Fail on bad data', function (done) {
